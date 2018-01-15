6bf60fc76c785d970e28823a146216ae5d1fc1e8
ItsCodingNix <35217330+ItsCodingNix@users.noreply.github.com>
2018-01-15 10:11:17 +0100

    Update main.py
    eliminacion de plt.axis ya que es inecesario.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 #esto deja que puedas poner los maximos  minimos valors del axis de valores
 xmin=int(input("valor minim de x?"))
 xmax=int(input("valor maxim de x?"))
-plt.axis([xmin,xmax])
+#plt.axis([xmin,xmax,ymin,ymax]) no hace falta plt.axis ya que te lo hace solo con lo de plot.
 plt.plot([0,1,2,3,4], [0,2,4,8,10], "-ro")
 plt.ylabel('Grafic de funcions')
 plt.savefig('graph.jpg')
