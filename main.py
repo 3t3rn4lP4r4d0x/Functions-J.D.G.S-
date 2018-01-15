d8b453187807f68e3e16b3b7e4191af8855c8da6
ItsCodingNix <35217330+ItsCodingNix@users.noreply.github.com>
2018-01-15 09:51:30 +0100

    update main.py
    he sacado la y max/min

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import matplotlib.pyplot as plt
 #esto deja que puedas poner los maximos  minimos valors del axis de valores
-xmin=eval(input("valor minim de x?"))
-xmax=eval(input("valor maxim de x?"))
-ymin=eval(input("valor minim de y?"))
-ymax=eval(input("valor maxim de y?"))
-plt.axis([xmin,xmax,ymin,ymax])
+xmin=int(input("valor minim de x?"))
+xmax=int(input("valor maxim de x?"))
+plt.axis([xmin,xmax])
 plt.plot([0,1,2,3,4], [0,2,4,8,10], "-ro")
 plt.ylabel('Grafic de funcions')
 plt.savefig('graph.jpg')
